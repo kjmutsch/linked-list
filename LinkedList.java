560dbfc87dcba2cd620aad15138d785bc6b5d22d
Kiara Mutschler <43933594+kjmutsch@users.noreply.github.com>
2025-04-10 16:11:11 -0500

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -20,28 +20,67 @@ public class LinkedList {
  
     // Insert a new node at the beginning of the list
     public void insertAtHead(int data) {
-        // Implementation goes here
+        Node temp = new Node(data);
+        temp.next = head;
+        head = temp;
     }
  
     // Insert a new node at the end of the list
     public void insertAtTail(int data) {
-        // Implementation goes here
+        // loop through entire list until you reach null
+        Node temp = new Node(data);
+
+        if(head == null) {
+            head = temp;
+            return;
+        }
+
+        Node current = head;
+        while(current.next != null) current = current.next; // we've now reached the last node in the list
+
+        current.next = temp;
     }
  
     // Delete a node with a specific value
     public void deleteNode(int data) {
-        // Implementation goes here
+        if(head.data == data) {
+            head = head.next;
+            return;
+        }
+        Node prev = head;
+        Node current = head.next;
+        while(current != null) {
+            if(current.data == data) {
+                // this means we found it
+                prev.next = current.next;
+                current = null; // this isn't necessary because of garbage collection I just like to explicitly set it
+                return;
+            }
+            prev = current;
+            current = current.next;
+        }
+        System.out.println("Could not find Node to delete");      
     }
  
     // Search for a node with a specific value
     public boolean search(int data) {
-        // Implementation goes here
-        return false; // Placeholder return value
+        Node current = head;
+        while(current != null) {
+            if(current.data == data) return true;
+            current = current.next;
+        }
+        return false;
     }
  
     // Traverse and print all elements in the list
     public void traverse() {
-        // Implementation goes here
+        Node current = head;
+        while(current != null) {
+            System.out.print(current.data + "-->");
+            current = current.next;
+        }
+        // tail is always null
+        System.out.println("null");
     }
  
     // Detect if there is a cycle in the list
